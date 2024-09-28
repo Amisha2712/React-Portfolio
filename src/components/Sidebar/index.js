e201b784da8682819d2ee971c5681a0104618bb1
Amisha2712 <jain2712amisha@gmail.com>
2024-09-28 22:24:44 +0530

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,47 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-a.png'
+import LogoSubtitle from '../../assets/images/logo_sub.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt='Amisha' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/amisha-jain-9175a11a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            
+            <li>
+                <a target='_blank' 
+                rel='noreferrer' 
+                href=''
+                >
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
